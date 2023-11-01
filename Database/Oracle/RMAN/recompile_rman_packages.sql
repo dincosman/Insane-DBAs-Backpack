a6e6aa066c6d9d637734e231224a3e8f293afb07
dincosman <69479099+dincosman@users.noreply.github.com>
2023-11-01 18:18:08 +0300

    Create recompile_rman_packages.sql

---
+++ Database/Oracle/RMAN/recompile_rman_packages.sql
@@ -0,0 +1,6 @@
+$ sqlplus / as sysdba
+
+SQL> @$ORACLE_HOME/rdbms/admin/dbmsrman.sql
+SQL> @$ORACLE_HOME/rdbms/admin/dbmsbkrs.sql
+SQL> @$ORACLE_HOME/rdbms/admin/prvtrmns.plb
+SQL> @$ORACLE_HOME/rdbms/admin/prvtbkrs.plb
