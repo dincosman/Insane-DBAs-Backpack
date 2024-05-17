34b39931e052280b7b699bcdfca96f32d8f72bd5
Osman DINC <69479099+dincosman@users.noreply.github.com>
2024-05-17 11:25:42 +0300

    Create 1mri_oracle.js

---
+++ Database/MongoDB/1MRI/1mri_oracle.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const mongodb = require("mongodb").MongoClient;
+const fastimp = require("fast-csv");
+
+// Start timing
+const startTime = process.hrtime();
+
+// Mongo API connection string
+let url = "mongodb://insanedba:bjk@192.168.1.31:27003/insanedba?authMechanism=PLAIN&authSource=$external&tlsInsecure=true&ssl=true&retryWrites=false&loadBalanced=true";
+let stream = fs.createReadStream("measurements.txt");
+let statData = [];
+let fastream = fastimp
+  .parse({delimiter: ';'})
+  .on("data", function(data) {
+    statData.push({
+      station_name: data[0],
+      measurement: data[1]
+    });
+  })
+  .on("end", function() {
+
+    mongodb.connect(
+      url,
+      { useNewUrlParser: true, useUnifiedTopology: true},
+      (err, client) => {
+        if (err) throw err;
+
+        client
+          .db("insanedba")
+          .collection("posts")
+          .insertMany(statData, (err, res) => {
+            if (err) throw err;
+
+            console.log(`Inserted: ${res.insertedCount} rows`);
+
+            // End timing
+            const endTime = process.hrtime(startTime);
+            const elapsedSeconds = endTime[0];
+            const elapsedNanoseconds = endTime[1];
+            const elapsedTimeInMilliseconds = (elapsedSeconds * 1000) + (elapsedNanoseconds / 1000000);
+            console.log(`Operation took: ${elapsedTimeInMilliseconds} milliseconds`);
+            client.close();
+          });
+      }
+    );
+  });
+
+stream.pipe(fastream);
+
